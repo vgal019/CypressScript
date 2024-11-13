62df68808fe300e7f010193c4d9eea80f9d553c2
vgal019 <vaishaligal019@gmail.com>
2024-11-13 17:35:53 +0530

    commit for cypressscript

--- cypress/Integration/example/GitTest.js
+++ cypress/Integration/example/GitTest.js
@@ -2,7 +2,7 @@ describe('Add Category for Interview', function() {
     it('First Test Case', function(){
 
 
- console.log("Edited in clone project by user2 ");
+ console.log("Edited in clone project by user2 test");
 
 
 
