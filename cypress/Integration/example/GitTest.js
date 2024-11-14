3069fb9b15e721db4999723307a0cf0ac0068ef2
vgal019 <vaishaligal019@gmail.com>
2024-11-14 11:42:30 +0530

    gitproject from develop branch

--- cypress/Integration/example/GitTest.js
+++ cypress/Integration/example/GitTest.js
@@ -4,7 +4,7 @@ describe('Add Category for Interview', function() {
 
  console.log("Edited in clone project by user2 test");
 console.log("editing from develop branch")
-
+console.log("editing from develop branch2")
 
     })
 
