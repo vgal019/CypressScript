6384ef506212e4b3201423e292d614de25c3660a
vgal019 <vaishaligal019@gmail.com>
2024-11-14 11:52:15 +0530

    updated gitproject final

--- cypress/Integration/example/GitTest.js
+++ cypress/Integration/example/GitTest.js
@@ -10,7 +10,7 @@ describe('Add Category for Interview', function() {
  console.log("Edited in clone project by user2 test");
 console.log("editing from develop branch")
 console.log("editing from develop branch2")
-
+//test test test test test
 
     })
 
